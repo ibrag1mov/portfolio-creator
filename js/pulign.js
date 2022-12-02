a599f1513c31e1ec068f1da0ff13c709833186d4
ibrag1mov <ibragimov.abdulhay@mail.ru>
2022-12-02 15:37:31 +0500

    services connected

---
+++ js/pulign.js
@@ -0,0 +1,9 @@
+//=== UNIVERSAL SELECTOR FUNCTION ====///
+
+const $=function(selector){
+  return document.querySelector(selector)
+}
+
+const $$=function(selector){
+  return document.querySelectorAll(selector)
+}
